70754c508bed2630f9d24009007cf7357c9da51d
Goldragon <16520417@std.stei.itb.ac.id>
2022-03-26 04:19:16 +0700

    +komen

--- src/main/java/com/monstersaku/Condition.java
+++ src/main/java/com/monstersaku/Condition.java
@@ -8,26 +8,29 @@ public interface Condition {
 }
 
 /*
-public class Condition extends Stats implements ConditionInterface{
-
+    //implementasi Interface Condition
     public void burn(){
         healthPoint = 0.875 * healthPoint;
         attack = 0.5 * attack;
     }
-
+    // catatan: aing bingung dapet max hpnya dari mana wkwk
     public void poison(){
         healthPoint = 0.9375 * healthPoint;
     }
-
+    // idem
     public int sleep(){
         Random dice = new Random();
         int number;
         number = 1+dice.nextInt(7);
         return number;
     }
-
-    public void paralyze(){
+    // number yang dihasilkan sleep menunjukan berapa ronde monster akan sleep
+    public int paralyze(){
         speed = 0.5 * speed;
+        Random dice = new Random();
+        int number;
+        number = 1+dice.nextInt(4);
+        return number;
     }
-}
+    //number yang dihasilkan menunjukan kemungkinan monster skip ronde (co: number 1 monster skip, 234 tidak skip)
 */
