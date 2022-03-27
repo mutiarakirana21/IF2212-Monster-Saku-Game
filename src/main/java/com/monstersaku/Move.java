92b6172db97216b176f9712fdedab54d358491be
Goldragon <16520417@std.stei.itb.ac.id>
2022-03-27 19:12:09 +0700

    Mengimplementasikan buff di kelas move

--- src/main/java/com/monstersaku/Move.java
+++ src/main/java/com/monstersaku/Move.java
@@ -8,14 +8,16 @@ public abstract class Move {
     private int accuracy;
     private int priority;
     private int ammunition;
+    private int statBuff;
 
     //konstruktor move
-    public Move(String name, ElementType elementType, int accuracy, int priority, int ammunition){
+    public Move(String name, ElementType elementType, int accuracy, int priority, int ammunition, int statBuff){
         this.name = name; 
         this.elementType = elementType; 
         this.accuracy = accuracy; 
         this.priority = priority; 
         this.ammunition = ammunition;
+        this.statBuff = statBuff;
     }
     
     //abstract class yang harus diimplementasikan di sub-class
@@ -40,6 +42,10 @@ public abstract class Move {
 
     public int getAmmunition(){
         return this.ammunition;
+    }
+
+    public int getStatBuff(){
+        return this.statBuff;
     }
 
     //setter
@@ -75,4 +81,119 @@ public abstract class Move {
         return exfirst;
     }
 
+    //method buff
+    public void setAttackBuff(int attack){
+        int x = this.getStatBuff();
+        if(x==-4){
+            attack = attack * 2/6;
+        }else if(x==-3){
+            attack = attack * 2/5;
+        }else if(x==-2){
+            attack = attack * 2/4;
+        }else if(x==-1){
+            attack = attack * 2/3;
+        }else if(x==0){
+            attack = attack * 1;
+        }else if(x==1){
+            attack = attack * 3/2;
+        }else if(x==2){
+            attack = attack * 4/2;
+        }else if(x==3){
+            attack = attack * 5/2;
+        }else if(x==4){
+            attack = attack * 6/2;
+        }
+    }
+
+    public void setDefenseBuff(int defense){
+        int x = this.getStatBuff();
+        if(x==-4){
+         defense = defense * 2/6;
+        }else if(x==-3){
+         defense = defense * 2/5;
+        }else if(x==-2){
+         defense = defense * 2/4;
+        }else if(x==-1){
+         defense = defense * 2/3;
+        }else if(x==0){
+         defense = defense * 1;
+        }else if(x==1){
+         defense = defense * 3/2;
+        }else if(x==2){
+         defense = defense * 4/2;
+        }else if(x==3){
+         defense = defense * 5/2;
+        }else if(x==4){
+         defense = defense * 6/2;
+        }
+    }
+
+    public void setSpecialAttackBuff(int specialAttack){
+        int x = this.getStatBuff();
+        if(x==-4){
+         specialAttack = specialAttack * 2/6;
+        }else if(x==-3){
+         specialAttack = specialAttack * 2/5;
+        }else if(x==-2){
+         specialAttack = specialAttack * 2/4;
+        }else if(x==-1){
+         specialAttack = specialAttack * 2/3;
+        }else if(x==0){
+         specialAttack = specialAttack * 1;
+        }else if(x==1){
+         specialAttack = specialAttack * 3/2;
+        }else if(x==2){
+         specialAttack = specialAttack * 4/2;
+        }else if(x==3){
+         specialAttack = specialAttack * 5/2;
+        }else if(x==4){
+         specialAttack = specialAttack * 6/2;
+        }
+    }
+
+    public void setSpecialDefenseBuff(int specialDefense){
+        int x = this.getStatBuff();
+        if(x==-4){
+         specialDefense = specialDefense * 2/6;
+        }else if(x==-3){
+         specialDefense = specialDefense * 2/5;
+        }else if(x==-2){
+         specialDefense = specialDefense * 2/4;
+        }else if(x==-1){
+         specialDefense = specialDefense * 2/3;
+        }else if(x==0){
+         specialDefense = specialDefense * 1;
+        }else if(x==1){
+         specialDefense = specialDefense * 3/2;
+        }else if(x==2){
+         specialDefense = specialDefense * 4/2;
+        }else if(x==3){
+         specialDefense = specialDefense * 5/2;
+        }else if(x==4){
+         specialDefense = specialDefense * 6/2;
+        }
+    }
+
+    public void setSpecialSpeed(int speed){
+        int x = this.getStatBuff();
+        if(x==-4){
+         speed = speed * 2/6;
+        }else if(x==-3){
+         speed = speed * 2/5;
+        }else if(x==-2){
+         speed = speed * 2/4;
+        }else if(x==-1){
+         speed = speed * 2/3;
+        }else if(x==0){
+         speed = speed * 1;
+        }else if(x==1){
+         speed = speed * 3/2;
+        }else if(x==2){
+         speed = speed * 4/2;
+        }else if(x==3){
+         speed = speed * 5/2;
+        }else if(x==4){
+         speed = speed * 6/2;
+        }
+    }
 }
