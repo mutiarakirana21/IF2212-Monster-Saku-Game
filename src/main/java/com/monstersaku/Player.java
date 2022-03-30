10a5fad81a16776870c3962814691f57b4369622
mutiarakirana21 <18220084@std.stei.itb.ac.id>
2022-03-30 14:50:13 +0700

    Create Player.java

---
+++ src/main/java/com/monstersaku/Player.java
@@ -0,0 +1,11 @@
+package com.monstersaku;
+
+import java.util.List;
+
+public class Player {
+    private String name;
+    private List<Monster> monster;
+    private Monster currentmons;
+
+    public Player(String name)
+}
